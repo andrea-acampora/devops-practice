6fc5d019ea56151363e75b279100caa69afc9b1d
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-12-14 16:02:23 +0100

    chore: add gradle catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
     application
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "io.github.andrea-acampora"
-
+val projectId = "$group.$name"
+val fullName = "Template project for DevOps Course"
+val projectDetails = "A template project with DevOps practice"
 repositories {
     mavenCentral()
 }
