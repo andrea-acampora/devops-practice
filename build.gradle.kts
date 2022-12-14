e18ea75124c8f26580bc0dabd90446734bfbe4b8
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-12-14 15:52:37 +0100

    chore: add hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.6.20"
+    application
 }
 
 group = "io.github.andrea-acampora"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
     testImplementation(kotlin("test"))
 }
 
@@ -19,6 +18,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+application {
+    mainClass.set("it.unibo.devops.MainKt")
 }
--- src/main/kotlin/it/unibo/devops/Main.kt
+++ src/main/kotlin/it/unibo/devops/Main.kt
@@ -1,3 +1,5 @@
+package it.unibo.devops
+
 fun main() {
     println("Hello, World!")
 }
