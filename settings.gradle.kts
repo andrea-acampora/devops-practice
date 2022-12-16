1602476e24a48e329ad4eb0866861d3b134a90ed
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-12-16 15:49:38 +0100

    chore: add enforce to use conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,22 @@
+plugins {
+    id("com.gradle.enterprise") version "3.11.1"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
 rootProject.name = "devops-practice"
 
 enableFeaturePreview("VERSION_CATALOGS")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
