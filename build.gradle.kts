e30b3e885e40d14df0a29d523b7278ffc3d4ee2a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-12-16 17:22:29 +0100

    chore: add puglin to publish on central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     application
     alias(libs.plugins.kotlin.jvm)
+//    alias(libs.plugins.publishOnCentral)
 }
 
 group = "io.github.andrea-acampora"
+
 val projectId = "$group.$name"
 val fullName = "Template project for DevOps Course"
 val projectDetails = "A template project with DevOps practice"
@@ -23,3 +25,40 @@ tasks.test {
 application {
     mainClass.set("it.unibo.devops.MainKt")
 }
+
+//signing {
+//    val signingKey: String? by project
+//    val signingPassword: String? by project
+//    useInMemoryPgpKeys(signingKey, signingPassword)
+//}
+//
+//val repoSlug = "andrea-acampora/devops-practice"
+//
+//publishOnCentral {
+//    projectDescription.set("DevOps practice project")
+//    projectLongName.set("DevOps Practice for LSS-2022")
+//    licenseName.set("Apache 2.0")
+//    licenseUrl.set("https://github.com/$repoSlug/blob/main/LICENSE.md")
+//    scmConnection.set("git:git@github.com:$repoSlug.git")
+//    repository("https://maven.pkg.github.com/${repoSlug.toLowerCase()}") {
+//        user.set("andrea-acampora")
+//        password.set(System.getenv("GITHUB_TOKEN"))
+//    }
+//}
+//
+//publishing {
+//    publications {
+//        withType<MavenPublication>() {
+//            pom {
+//                developers {
+//                    developer {
+//                        name.set("Andrea Acampora")
+//                        email.set("andrea.acampora@studio.unibo.it")
+//                        url.set("http://andrea-acampora.github.io")
+//                        roles.set(mutableSetOf("architect", "developer"))
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
