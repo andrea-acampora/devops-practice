d2b08c750dda527a38a6e91f386ebd4ea8ee1c75
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-01-09 16:46:14 +0100

    chore: modify gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     alias(libs.plugins.kotlin.jvm)
-//    alias(libs.plugins.publishOnCentral)
+    alias(libs.plugins.publishOnCentral)
 }
 
 group = "io.github.andrea-acampora"
@@ -26,39 +26,39 @@ application {
     mainClass.set("it.unibo.devops.MainKt")
 }
 
-//signing {
-//    val signingKey: String? by project
-//    val signingPassword: String? by project
-//    useInMemoryPgpKeys(signingKey, signingPassword)
-//}
-//
-//val repoSlug = "andrea-acampora/devops-practice"
-//
-//publishOnCentral {
-//    projectDescription.set("DevOps practice project")
-//    projectLongName.set("DevOps Practice for LSS-2022")
-//    licenseName.set("Apache 2.0")
-//    licenseUrl.set("https://github.com/$repoSlug/blob/main/LICENSE.md")
-//    scmConnection.set("git:git@github.com:$repoSlug.git")
-//    repository("https://maven.pkg.github.com/${repoSlug.toLowerCase()}") {
-//        user.set("andrea-acampora")
-//        password.set(System.getenv("GITHUB_TOKEN"))
-//    }
-//}
-//
-//publishing {
-//    publications {
-//        withType<MavenPublication>() {
-//            pom {
-//                developers {
-//                    developer {
-//                        name.set("Andrea Acampora")
-//                        email.set("andrea.acampora@studio.unibo.it")
-//                        url.set("http://andrea-acampora.github.io")
-//                        roles.set(mutableSetOf("architect", "developer"))
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+}
+
+val repoSlug = "andrea-acampora/devops-practice"
+
+publishOnCentral {
+    projectDescription.set("DevOps practice project")
+    projectLongName.set("DevOps Practice for LSS-2022")
+    licenseName.set("Apache 2.0")
+    licenseUrl.set("https://github.com/$repoSlug/blob/main/LICENSE.md")
+    scmConnection.set("git:git@github.com:$repoSlug.git")
+    repository("https://maven.pkg.github.com/${repoSlug.toLowerCase()}") {
+        user.set("andrea-acampora")
+        password.set(System.getenv("GITHUB_TOKEN"))
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication>() {
+            pom {
+                developers {
+                    developer {
+                        name.set("Andrea Acampora")
+                        email.set("andrea.acampora@studio.unibo.it")
+                        url.set("http://andrea-acampora.github.io")
+                        roles.set(mutableSetOf("architect", "developer"))
+                    }
+                }
+            }
+        }
+    }
+}
